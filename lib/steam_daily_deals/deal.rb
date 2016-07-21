b88e15ffe947cd159bf3b992c07d622a993c3a89
arturusfury <bri.reynolds@gmail.com>
2016-07-20 19:57:49 -0500

    added basic structure for a deal

--- lib/steam_daily_deals/deal.rb
+++ lib/steam_daily_deals/deal.rb
@@ -1,4 +1,24 @@
 # Deal Class
-class Deal
+class SteamDailyDeals::Deal
+  attr_accessor :name,
+                :final_price,
+                :app_url,
+                :description,
+                :release_date,
+                :ratings,
+                :popular_tags
 
+  @@all = []
+
+  def initialize(deal)
+    deal.each do |key, value|
+      send("#{key}=", value)
+    end
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
