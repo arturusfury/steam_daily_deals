ac40f2d46da9cac0213696269ef1074d4d8717bb
arturusfury <bri.reynolds@gmail.com>
2016-07-21 18:55:55 -0500

    added dependcies for selenium, phantomjs and set phantomjs path

--- lib/steam_daily_deals.rb
+++ lib/steam_daily_deals.rb
@@ -1,9 +1,13 @@
 require 'pry'
 require 'nokogiri'
-require 'open-uri'
 require 'command_line_reporter'
+require 'selenium-webdriver'
+require 'phantomjs'
+# require 'open-uri'
 
 require_relative './steam_daily_deals/version'
 require_relative '../lib/steam_daily_deals/cli'
 require_relative '../lib/steam_daily_deals/deal'
 require_relative '../lib/steam_daily_deals/scraper'
+
+Selenium::WebDriver::PhantomJS.path = Phantomjs.path
