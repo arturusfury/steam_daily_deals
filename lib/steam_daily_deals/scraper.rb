846e86b9afd6adb3831fa52194f4699e851e33a0
arturusfury <bri.reynolds@gmail.com>
2016-07-21 13:53:09 -0500

    added in scraping functionality for the index page

--- lib/steam_daily_deals/scraper.rb
+++ lib/steam_daily_deals/scraper.rb
@@ -1,7 +1,20 @@
 # Scraper class
 class SteamDailyDeals::Scraper
   def self.scrape_index_page(index_url)
-    # Stuff
+    index_page = Nokogiri::HTML(open(index_url))
+
+    deals = []
+
+    index_page.css('div.cluster_scroll_area').each do |card|
+      card.css('a.cluster_capsule').each do |deal|
+        app_url = deal.attr('href').to_s
+        final_price = deal.css('.discount_final_price').text
+        availibility = deal.css('.main_cap_status').text
+        deals << { app_url: app_url, final_price: final_price, availibility: availibility }
+      end
+    end
+
+    deals
   end
 
   def self.scrape_deal_page(page_url)
