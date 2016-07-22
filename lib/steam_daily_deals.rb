1c62be9942ca430fbdbeec9401df2940c511c5bb
arturusfury <bri.reynolds@gmail.com>
2016-07-21 19:40:47 -0500

    added open uri back in for scraping the deal pages

--- lib/steam_daily_deals.rb
+++ lib/steam_daily_deals.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require 'command_line_reporter'
 require 'selenium-webdriver'
 require 'phantomjs'
-# require 'open-uri'
+require 'open-uri'
 
 require_relative './steam_daily_deals/version'
 require_relative '../lib/steam_daily_deals/cli'
