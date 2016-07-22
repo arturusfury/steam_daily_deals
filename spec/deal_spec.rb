95dd1829f0ca467d7cfe894cbf6a6cec236c3181
arturusfury <bri.reynolds@gmail.com>
2016-07-22 09:02:08 -0500

    started basic structure for more tests on our deal methods

--- spec/deal_spec.rb
+++ spec/deal_spec.rb
@@ -1,66 +1,47 @@
 describe 'Deal' do
-  let!(:daily_deal_array) do
+  let!(:deal_index_array) do
     [
       {
-        name: 'Grand Theft Auto V',
         final_price: '$40.19',
         app_url: 'spec/fixtures/Grand_Theft_Auto_V.htm',
-        availibility: 'Now Available',
-        description: 'GTA Online: Cunning Stunts -- Play Now. Check out a series of brand-new, high octane Stunt Races. Including all new vehicles, racing-themed clothing and more. Buckle up and prepare to experience Southern San Andreas from a whole new perspective.',
-        release_date: 'Apr 14, 2015',
-        recent_rating: 'Mostly Positive',
-        recent_reviews: '8,907',
-        overall_rating: 'Very Positive',
-        total_reviews: '127,740',
-        popular_tags: ['Open World', 'Action', 'Multiplayer', 'First-Person', 'Third Person', 'Crime', 'Adventure', 'Shooter', 'Third-Person Shooter', 'Singleplayer', 'Atmospheric', 'Mature', 'Racing', 'Sandbox', 'Co-op', 'Great Soundtrack', 'Funny', 'Comedy', 'Moddable', 'RPG']
+        availibility: 'Now Available'
       },
       {
-        name: 'Batman - The Telltale Series',
         final_price: '$22.49',
         app_url: 'spec/fixtures/Batman_The_Telltale_Series.htm',
-        availibility: 'Pre-Order',
-        description: 'Enter the fractured psyche of Bruce Wayne and discover the powerful and far-reaching consequences of your choices as the Dark Knight.',
-        overall_rating: 'No user reviews',
-        popular_tags: ['Batman', 'Adventure', 'Point & Click', 'Story Rich', 'Singleplayer', 'Choices Matter', 'Superhero', 'Action', 'Atmospheric', 'Episodic', 'Memes', 'Dating Sim', 'Quick-Time Events', 'Nudity', 'Dark', 'Anime', 'Comedy', 'Multiplayer']
+        availibility: 'Pre-Order'
       },
       {
-        name: 'Hive Jump',
         final_price: '$19.99',
         app_url: 'spec/fixtures/Hive_Jump.htm',
-        availibility: 'Early Access',
-        description: 'HIVE JUMP is a sci-fi action platformer for 1-4 players blending run ‘n gun gameplay with roguelike elements.',
-        release_date: 'Jul 20, 2016',
-        overall_rating: 'Positive',
-        total_reviews: '7',
-        popular_tags: ['Early Access', 'Action', 'Indie', 'Procedural Generation', 'Multiplayer', 'Local Co-op', 'Sci-fi', 'Aliens', 'Pixel Graphics', 'Co-op', 'Great Soundtrack', 'Arcade', 'Colorful', 'Platformer']
+        availibility: 'Early Access'
       },
       {
-        name: 'BrainBread 2',
         final_price: 'Free',
         app_url: 'spec/fixtures/BrainBread_2.htm',
-        availibility: 'Early Access',
-        description: 'Grab a weapon, demolish your enemies, level up, become more powerful, let the gore flow, let the limbs fly. BrainBread 2 introduces a zombie fps mixed with RPG / Arcade elements, the game is very action-packed and generally fast-paced.',
-        release_date: 'Jul 20, 2016',
-        overall_rating: 'Mixed',
-        total_reviews: '125',
-        popular_tags: ['Early Access', 'Free to Play', 'Action', 'Indie', 'Zombies', 'Adventure', 'Arcade', 'Great Soundtrack', 'RPG', 'Action RPG', 'Classic', 'Multiplayer']
+        availibility: 'Early Access'
       },
       {
-        name: 'Magic Duels',
         app_url: 'spec/fixtures/Magic_Duels.htm',
-        availibility: 'Free to Play',
-        description: 'MORE CARDS. MORE STRATEGY. BIGGER STORY. Playing Magic with your friends has never been more fun! Featuring more than 125 NEW unique earnable cards from Magic’s Eldritch Moon set, NEW Story Content, and more.',
-        release_date: 'Jul 29, 2015',
-        recent_rating: 'Mixed',
-        recent_reviews: '357',
-        overall_rating: 'Mixed',
-        total_reviews: '12,333',
-        popular_tags: ['Free to Play', 'Trading Card Game', 'Card Game', 'Strategy', 'Magic', 'Multiplayer', 'Co-op', 'Singleplayer', 'Fantasy', 'Online Co-Op']
+        availibility: 'Free to Play'
       }
     ]
   end
 
-  let!(:gta5) {SteamDailyDeals::Deal.new({name: "Grand Theft Auto 5", final_price: '$40.19', app_url: 'spec/fixtures/Grand_Theft_Auto_V.htm'})}
+  let!(:deal_hash) do
+    {
+      name: 'Grand Theft Auto V',
+      description: 'GTA Online: Cunning Stunts -- Play Now. Check out a series of brand-new, high octane Stunt Races. Including all new vehicles, racing-themed clothing and more. Buckle up and prepare to experience Southern San Andreas from a whole new perspective.',
+      release_date: 'Apr 14, 2015',
+      recent_rating: 'Mostly Positive',
+      recent_reviews: '8,907',
+      overall_rating: 'Very Positive',
+      total_reviews: '127,740',
+      popular_tags: ['Open World', 'Action', 'Multiplayer', 'First-Person', 'Third Person', 'Crime', 'Adventure', 'Shooter', 'Third-Person Shooter', 'Singleplayer', 'Atmospheric', 'Mature', 'Racing', 'Sandbox', 'Co-op', 'Great Soundtrack', 'Funny', 'Comedy', 'Moddable', 'RPG']
+    }
+  end
+
+  let!(:gta5) {SteamDailyDeals::Deal.new({final_price: '$40.19', app_url: 'spec/fixtures/Grand_Theft_Auto_V.htm'})}
 
   after(:each) do
     SteamDailyDeals::Deal.class_variable_set(:@@all, [])
@@ -69,9 +50,27 @@ describe 'Deal' do
   describe '#initialize' do
     it 'accepts a hash of attributes and creates a deal object' do
       expect{SteamDailyDeals::Deal.new({name: 'Magic Duels', app_url: 'spec/fixtures/Magic_Duels.htm'})}.to_not raise_error
-      expect(gta5.name).to eq('Grand Theft Auto 5')
       expect(gta5.final_price).to eq('$40.19')
       expect(gta5.app_url).to eq('spec/fixtures/Grand_Theft_Auto_V.htm')
+    end
+
+    it 'expects that the newly created deal class is added to our collection of deals stored in @@all' do
+      expect(SteamDailyDeals::Deal.all.first.name).to eq('Grand Theft Auto V')
+    end
+  end
+
+  describe '.create_from_collection' do
+    it 'uses our scraper classes to grab the data and create new deals' do
+      SteamDailyDeals::Deal.class_variable_set(:@@all, [])
+      SteamDailyDeals::Deal.create_from_collection(deal_index_array)
+      epect(SteamDailyDeals::Deal.class_variable_get.last.app_url).to eq('spec/fixtures/Magic_Duels.htm')
+    end
+  end
+
+  describe '#add_deal_information' do
+    it 'uses our scraper classes to grab all the information on the deal' do
+      gta5.add_deal_information(deal_hash)
+      expect(gta5.name).to equal('Grand Theft Auto V')
     end
   end
 
